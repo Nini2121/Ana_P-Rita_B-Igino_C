d3d9c2ee426af1eb2d3fb89eef7c6b2294e5c41e
Igino Contin <continigino@gmail.com>
2022-11-02 09:46:05 +0000

    drawing shapes

--- ar_paint.py
+++ ar_paint.py
@@ -1,6 +1,8 @@
-#para os argumentos iniciais dos limites
 import argparse
 from copy import deepcopy
+from operator import index
+from re import T
+from xmlrpc.client import TRANSPORT_ERROR
 import cv2
 from textwrap import indent
 import json
@@ -37,11 +39,9 @@ def main():
     lower_bound=np.array([min_B,min_G,min_R])
     upper_bound=np.array([max_B,max_G,max_R])
 
-    # Giving different arrays to handle colour points of different colours
-    bpoints = []
-    gpoints = []
-    rpoints = []
-
+    rgb_points=[]
+    thick_points=[]
+    color_points=[]
     thickness=1
 
     #starting the painting window setup
@@ -51,8 +51,18 @@ def main():
     kernel = np.ones((10,10),np.uint8)
 
     paintWindow = np.zeros((471,636,3)) + 255
+    paintWindow = cv2.flip(paintWindow, 1)
 
     center = None
+
+    square_mode=False
+    ellipse_mode=False
+    first_press=False
+    debouncing=0
+    last_point_rect=[]
+    last_point_circle=[]
+    my_rect=[]
+    my_circle=[]
 
     video_capture = cv2.VideoCapture(0)
 
@@ -61,11 +71,13 @@ def main():
         _, frame = video_capture.read()
         vid_thresh = frame.copy()
         vid_mask=cv2.inRange(vid_thresh,lower_bound, upper_bound)
+        frame = cv2.flip(frame, 1)
 
         #add some dialation to increase segmented area
         Mask = cv2.erode(vid_mask, kernel, iterations=1)
         Mask = cv2.morphologyEx(Mask, cv2.MORPH_OPEN, kernel)
         Mask = cv2.dilate(Mask, kernel, iterations=1)
+        Mask = cv2.flip(Mask, 1)
         
         #find all the contours of the segmented mask
         cnts,_ = cv2.findContours(Mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
@@ -85,47 +97,86 @@ def main():
             M = cv2.moments(cnt)
             center = (int(M['m10'] / M['m00']), int(M['m01'] / M['m00']))
 
-            if colorIndex == 0:
-                bpoints.append(center)
-                if len(bpoints)>2:
-                    p1=bpoints[-2]
-                    p2=bpoints[-1]
-                    cv2.line(paintWindow, p1, p2, colors[colorIndex], thickness)
+            rgb_points.append(center)
+            #store for each point also the color and the thickness
+            color_points.append(colorIndex)
+            thick_points.append(thickness)
 
-            elif colorIndex == 1:
-                gpoints.append(center)
-                if len(gpoints)>2:
-                    p1=gpoints[-2]
-                    p2=gpoints[-1]
-                    cv2.line(paintWindow,p1 ,p2 , colors[colorIndex], thickness)
+            # draw stuff
+            if len(rgb_points) > 2 :
+                # if square_mode == False and ellipse_mode == False:
+                #     p1=rgb_points[-2]
+                #     p2=rgb_points[-1]
+                #     #cv2.line(paintWindow,p1 ,p2 , colors[colorIndex], thickness)
+                # if square_mode == True:
+                #     cv2.rectangle(paintWindow, actual_point, rgb_points[-1], colors[colorIndex], thickness)
+                # if ellipse_mode == True:
+                #     radius= max(abs(actual_point[0]-rgb_points[-1][0]),abs(actual_point[1]-rgb_points[-1][1]))
+                #     cv2.circle(paintWindow, actual_point, radius, colors[colorIndex], thickness)
+                
+                #clean and draw every time the line, only if activated draw rect or circle
 
-            elif colorIndex == 2:
-                rpoints.append(center)
-                if len(rpoints)>2:
-                    p1=rpoints[-2]
-                    p2=rpoints[-1]
-                    cv2.line(paintWindow,p1 ,p2 , colors[colorIndex], thickness)
+                if square_mode == False and ellipse_mode == False:
+                    paintWindow = np.zeros((471,636,3)) + 255
+                    for k in range(1,len(rgb_points)-1):
+                        if max(abs(rgb_points[k-1][0]-rgb_points[k][0]),abs(rgb_points[k-1][1]-rgb_points[k][1])) > 30 : #if two points are too much distant is an error
+                            continue
+                        cv2.line(paintWindow, rgb_points[k - 1], rgb_points[k], colors[color_points[k]], thick_points[k])
+                    for k in my_rect:
+                        cv2.rectangle(paintWindow, k[0], k[1], colors[k[2]], k[3])
+                    for k in my_circle:
+                        cv2.circle(paintWindow, k[0],k[1], colors[k[2]], k[3])
 
-        
-        #in this way every frame you re-paint the whole vector of points------------->is correct to draw every frame
+                if square_mode == True:
+                    paintWindow = np.zeros((471,636,3)) + 255
+                    for k in range(1,rgb_points.index(actual_point)):#redraw everything
+                        if max(abs(rgb_points[k-1][0]-rgb_points[k][0]),abs(rgb_points[k-1][1]-rgb_points[k][1])) > 30 : #if two points are too much distant is an error
+                            continue
+                        cv2.line(paintWindow, rgb_points[k - 1], rgb_points[k], colors[color_points[k]], thick_points[k])
+                    for k in my_rect:
+                        cv2.rectangle(paintWindow, k[0], k[1], colors[k[2]], k[3])
+                    for k in my_circle:
+                        cv2.circle(paintWindow, k[0],k[1],colors[k[2]], k[3])
 
-        # points = [bpoints, gpoints, rpoints] 
-        # for i in range(len(points)):
-        #     for j in range(len(points[i])):
-        #         for k in range(1, len(points[i][j])):
-        #             if points[i][j][k - 1] is None or points[i][j][k] is None:
-        #                 continue
-        #             cv2.line(frame, points[i][j][k - 1], points[i][j][k], colors[i], thickness)
-        #             cv2.line(paintWindow, points[i][j][k - 1], points[i][j][k], colors[i], thickness)
+                    cv2.rectangle(paintWindow, actual_point, rgb_points[-1], colors[colorIndex], thickness)
+                    pt=(actual_point,rgb_points[-1], colorIndex, thickness)
+                    last_point_rect.append(pt)
+                    rgb_points.pop(-1) #avoid to draw line while drawing figure
+                    color_points.pop(-1)
+                    thick_points.pop(-1)
+
+                if ellipse_mode == True:
+                    paintWindow = np.zeros((471,636,3)) + 255
+                    for k in range(1,rgb_points.index(actual_point)):#redraw everything
+                        if max(abs(rgb_points[k-1][0]-rgb_points[k][0]),abs(rgb_points[k-1][1]-rgb_points[k][1])) > 30 : #if two points are too much distant is an error
+                            continue
+                        cv2.line(paintWindow, rgb_points[k - 1], rgb_points[k], colors[color_points[k]], thick_points[k])
+                    for k in my_rect:
+                        cv2.rectangle(paintWindow, k[0], k[1], colors[k[2]], k[3])
+                    for k in my_circle:
+                        cv2.circle(paintWindow, k[0],k[1], colors[k[2]], k[3])
+
+                    radius= max(abs(actual_point[0]-rgb_points[-1][0]),abs(actual_point[1]-rgb_points[-1][1]))
+                    cv2.circle(paintWindow, actual_point, radius, colors[colorIndex], thickness)
+                    pt=(actual_point, radius, colorIndex, thickness)
+                    last_point_circle.append(pt)
+                    rgb_points.pop(-1) #avoid to draw line while drawing figure
+                    color_points.pop(-1)
+                    thick_points.pop(-1)
+
+
+
 
         k=cv2.waitKey(1)
         if k == 99:  # c clean the drawing
-            print('clean paint')
-            bpoints = []
-            gpoints = []
-            rpoints = []
+            print('clear paint window')
+            rgb_points=[]
+            color_points=[]
+            thick_points=[]
+            my_rect=[]
+            my_circle=[]
             paintWindow = np.zeros((471,636,3)) + 255
-
+        #change color
         elif k== ord('b'):
             print('change color: blue')
             colorIndex = 0      
@@ -135,7 +186,7 @@ def main():
         elif k == ord('r'):
             print('change color: red')
             colorIndex = 2
-
+        #thickness
         elif k == ord('+'):
             print('thickness +')
             thickness += 1
@@ -145,14 +196,51 @@ def main():
                 thickness -= 1
             else:
                 print('is the minimum thickness')
-
+        #write image
         elif k == ord('w'):
             print('write actual image')
             cv2.imwrite(time.ctime().replace(' ','_')+'.png',paintWindow)
+        #exit
         elif k ==ord('q'):
             print('exit')
             exit(0)
 
+        # drawing shapes
+        #debouncing to read correctly the pression on the key
+        elif k == ord('s'):# square drawing
+            debouncing+=1
+            if debouncing >= 1:
+                if first_press == False:
+                    first_press=True
+                    print('drawing rectangle mode')
+                    actual_point=center
+                square_mode=True
+                
+
+        elif k == ord('o'):# ellipse drawing(circle)
+            debouncing+=1
+            if debouncing >=1:
+                if first_press == False:
+                    first_press=True
+                    print('drawing ellipse mode')
+                    actual_point=center
+                ellipse_mode=True
+
+        else :
+            if debouncing>1:
+                debouncing=0
+                print('esc drawing mode')
+                if len(last_point_rect)>0:
+                    my_rect.append(last_point_rect[-1])
+                    last_point_rect=[]
+                if len(last_point_circle)>0:
+                    my_circle.append(last_point_circle[-1])
+                    last_point_circle=[]
+                first_press=False
+                square_mode=False
+                ellipse_mode=False
+
+        #paintWindow = cv2.flip(paintWindow, 1)
         cv2.imshow("Tracking", frame)
         cv2.imshow("Paint", paintWindow)
         cv2.imshow("mask",Mask)
